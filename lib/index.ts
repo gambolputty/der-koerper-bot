41414c854e2551f25f3c192d5f23b274f864ba18
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2024-05-09 15:16:14 +0200

    wip

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,9 @@
 import * as random from "./random";
-import type { GenerateTextProps, GenerateTextResult } from "./story";
+import type { Filters, GenerateTextProps, GenerateTextResult } from "./story";
 import { SentenceSchema, Story } from "./story";
 import type { SentenceType } from "./story/sentence";
 import { Trash, TrashMap } from "./trash";
 
 // eslint-disable-next-line
 export { SentenceSchema, Story, Trash, TrashMap, random };
-export type { GenerateTextProps, GenerateTextResult, SentenceType };
+export type { Filters, GenerateTextProps, GenerateTextResult, SentenceType };
